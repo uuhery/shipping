d51c0bf7e7bef5bc3bb09c12204ef8ec5c46fae4
Cinderella <20301115@bjtu.edu.cn>
2023-05-29 17:52:39 +0800

    小配了一下置

---
+++ src/main/java/com/company/project/common/config/CorsConfig.java
@@ -0,0 +1,47 @@
+package com.company.project.common.config;
+ 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+ 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+ 
+@Configuration
+public class CorsConfig extends WebMvcConfigurerAdapter {
+ 
+   @Override
+   public void addCorsMappings(CorsRegistry registry) {
+      // TODO Auto-generated method stub
+      // super.addCorsMappings(registry);
+      registry.addMapping("/**")
+         .allowedOrigins("*")
+         .allowCredentials(true)
+         .allowedMethods("GET", "POST", "DELETE", "PUT")
+         .maxAge(3600);
+   }
+ 
+   @Override
+   public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+ 
+      MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
+      //设置日期格式
+      ObjectMapper objectMapper = new ObjectMapper();
+       SimpleDateFormat smt = new SimpleDateFormat("yyyy-MM-dd");
+       objectMapper.setDateFormat(smt);
+      mappingJackson2HttpMessageConverter.setObjectMapper(objectMapper);
+      //设置中文编码格式
+      List<MediaType> list = new ArrayList<MediaType>();
+      list.add(MediaType.APPLICATION_JSON_UTF8);
+      mappingJackson2HttpMessageConverter.setSupportedMediaTypes(list);
+      converters.add(mappingJackson2HttpMessageConverter);
+      super.configureMessageConverters(converters);
+   }
+ 
+ 
+}
--- src/main/java/com/company/project/common/oauth2/SecurityConfig.java
+++ src/main/java/com/company/project/common/oauth2/SecurityConfig.java
@@ -2,14 +2,19 @@ package com.company.project.common.oauth2;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
+@Order(1)
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -35,6 +40,25 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .csrf()
                 .disable();
+        //http.headers().frameOptions().disable(); // 任何跨域
+        http.headers().frameOptions().sameOrigin(); // 同源跨域
+        http.csrf().disable()
+                .authorizeRequests()
+                .anyRequest().permitAll()
+                .and().logout().permitAll();
+
+        http.cors().configurationSource(corsConfigurationSource());
+
+    }
+
+    private CorsConfigurationSource corsConfigurationSource() {
+        CorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*"); // 同源配置，*表示任何请求都视为同源，若需指定ip和端口可以改为如“localhost：8080”，多个以“，”分隔；
+        corsConfiguration.addAllowedHeader("*");// header，允许哪些header，本案中使用的是token，此处可将*替换为token；
+        corsConfiguration.addAllowedMethod("*"); // 允许的请求方法，PSOT、GET等
+        ((UrlBasedCorsConfigurationSource) source).registerCorsConfiguration("/**", corsConfiguration); // 配置允许跨域访问的url
+        return source;
     }
 
 
